8a778524bc7f63a14e10415a0677e4b8e9494632
Will Dean <57733339+wd60622@users.noreply.github.com>
2022-02-14 15:31:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="take_notes",
     version="0.1.0",
     author="William Dean",
-    author_email="wdean@homepartners.com",
+    author_email="wd60622@gmail.com",
     description="Quickly take different notes.",
     packages=["take_notes"],
     scripts=["scripts/notes"],
